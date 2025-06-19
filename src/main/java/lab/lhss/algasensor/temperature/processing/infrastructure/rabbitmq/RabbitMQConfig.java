7941266950aa85f7a33c8e02c31874f2c7137039
Lucas Henrique <lucas.henrique0023@gmail.com>
2025-06-19 20:51:08 -0300

    Creating RabbitMQ Exchange With Spring AMQP

---
+++ src/main/java/lab/lhss/algasensor/temperature/processing/infrastructure/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,24 @@
+package lab.lhss.algasensor.temperature.processing.infrastructure.rabbitmq;
+
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public FanoutExchange fanoutExchange() {
+        return ExchangeBuilder.fanoutExchange("temperature-processing.temperature-received.v1.e")
+                .durable(true).build();
+    }
+
+}
---
+++ src/main/java/lab/lhss/algasensor/temperature/processing/infrastructure/rabbitmq/RabbitMQInitializer.java
@@ -0,0 +1,19 @@
+package lab.lhss.algasensor.temperature.processing.infrastructure.rabbitmq;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RabbitMQInitializer {
+
+    private final RabbitAdmin rabbitAdmin;
+
+    @PostConstruct
+    public void initialize() {
+        rabbitAdmin.initialize();
+    }
+
+}
